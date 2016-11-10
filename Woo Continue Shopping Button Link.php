bf14ce8215a95f41326b92e885d3c0eb440efed4
Brett Redvers <dollhousedigital@gmail.com>
2016-11-10 05:31:11 +0000

    Update Woo Continue Shopping Button Link.php

--- Woo Continue Shopping Button Link.php
+++ Woo Continue Shopping Button Link.php
@@ -1,3 +1,5 @@
+/*--CONTINUE SHOPPING BUTTON LINK--*/
+
 add_filter( 'woocommerce_continue_shopping_redirect', 'wc_custom_redirect_continue_shopping' );
 
 function wc_custom_redirect_continue_shopping() {
