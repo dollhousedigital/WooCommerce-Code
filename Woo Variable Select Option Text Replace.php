3b7fa7b8a1986818828cef3b97fe552a6dae1c9f
Brett Redvers <dollhousedigital@gmail.com>
2016-11-10 05:28:16 +0000

    Create Woo Variable Select Option Text Replace.php

---
+++ Woo Variable Select Option Text Replace.php
@@ -0,0 +1,7 @@
+add_filter('gettext', 'translate_text'); 
+add_filter('ngettext', 'translate_text');
+
+function translate_text($translated) { 
+$translated = str_ireplace('Choose an option', 'Name of the product option', $translated); 
+return $translated; 
+}
